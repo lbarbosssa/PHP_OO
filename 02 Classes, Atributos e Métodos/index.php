aa364c490059b3909b95df71ee8afb6f8a2a1941
Lucas Barbosa <barbosa.lucas1996@gmail.com>
2018-11-06 19:37:47 -0200

    02 Classes, Atributos e Métodos

--- 02 Classes, Atributos e Métodos/index.php
+++ 02 Classes, Atributos e Métodos/index.php
@@ -1,1 +1,27 @@
 
+<?php 
+
+	class Pessoa {
+		public $nome;
+		public $idade;
+	
+		public function Falar() {
+			echo "Falou <br>";
+		}
+
+		public function Falar_Completo() {
+			echo $this->nome. " de ".$this->idade." anos acabou de falar";
+		}
+	}
+
+
+$lucas = new Pessoa();
+$lucas->nome = "Lucas Barbosa";
+$lucas->idade = "22";
+$lucas->Falar();
+var_dump($lucas);
+echo "<br>";
+echo $lucas->nome;
+echo "<br>";
+$lucas->Falar_Completo();
+?>
